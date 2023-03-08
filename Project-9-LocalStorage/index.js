6c09ccc41b12a270388af69e35df946729b7bee1
Maria Angelova <angelova.m@hotmail.com>
2023-03-08 12:33:42 +0100

    Add saving checkbxs condition

--- Project-9-LocalStorage/index.js
+++ Project-9-LocalStorage/index.js
@@ -1,45 +1,75 @@
-/*function lS() {
-        for(let i = 0; i<localStorage.length; i++) {
-            const key = localStorage.key(i)
-            const value = localStorage.getItem(key)
-            console.log(key, value)
-    }
-}
-*/
+//  localStorage.getItem(key, value)
 
-const button = document.querySelector('input[type="button"]');
-const newToDoInput = document.querySelector('input[type="text"]');
-const list = document.querySelector("#list");
+
+const button = document.querySelector(".add");                      // кнопка добавления
+const clearButton = document.querySelector(".clear")                // кнопка удаления
+const newToDoInput = document.querySelector('input[type="text"]');  // текстовое поле
+const list = document.querySelector("#list");                       // форма со списком todo
+
 const array = localStorage.getItem("toDo") ? localStorage.getItem("toDo").split(",") : []; //state
 
+const createToDo = (text) => {   //функция создания туду в разметке
+    const newToDoContainer = document.createElement("label");   // создание лейбла 
+    const newToDoValue = document.createElement("input");       // создания инпута для чекбокса
+    const paragraph = document.createElement("p");              // создание p для текста
+    const deleteButton = document.createElement("input");       // создание инпута для кнопки удаления
+    
+    const dlt = event => {                                      // функция удаления элемента
+        newToDoContainer.remove();                              //удаление элемента
+        localStorage.setItem("toDo", array.filter(el => el !== paragraph.innerText).join(",")); //обновление хранилища
+    };
 
-const createToDo = (text) => {
-    const newToDoContainer = document.createElement("label");
-    const newToDoValue = document.createElement("input");
-    const paragraph = document.createElement("p");
+    const changeHandler = event => {        //функция сохранения состояния чекбоксов
+        if (newToDoValue.checked) {
+            localStorage.setItem(paragraph.innerText, JSON.stringify(newToDoValue.checked)) //newToDoValue.checked  - true !!
+        } else {
+            localStorage.setItem(paragraph.innerText, false)
+        }
+    }
 
-    newToDoValue.setAttribute("type", "checkbox");
-    paragraph.innerText = text;
-    newToDoContainer.append(newToDoValue, paragraph);
-    return newToDoContainer;
+    function winOnload() {                  // функция добавления сохраненных чекбоксов при загрузке страницы
+        newToDoValue.checked = localStorage.getItem(paragraph.innerText) === 'true' 
+    }
+
+    window.addEventListener("load", winOnload)
+
+    paragraph.innerText = text;    // добавление текста внутрь элемента p ч/з переменную        
+    
+    deleteButton.addEventListener("click", dlt);    // доб слушатель кнопке удаления с функцией удаления элемента
+    deleteButton.setAttribute("type", "button");    // доб атрибуты тип 
+    deleteButton.setAttribute("value", "x");            // и значение
+    
+    newToDoContainer.append(newToDoValue, paragraph, deleteButton);// добавляем checkbox и p внутрь label-контейнер
+    newToDoValue.setAttribute("type", "checkbox");  // добавление атрибутов у инпутов
+    newToDoValue.addEventListener("change", changeHandler);
+
+    return newToDoContainer; // возвращаем готовый label-контейнер;
 }
 
-const handleAdd = event => {
-    list.append(createToDo(newToDoInput.value));
-    array.push(newToDoInput.value);
-    localStorage.setItem("toDo",array.join(",")); // записываем массив в строку - serialisation
-    newToDoInput.value = "";
+const handleAdd = event => { //функция добавления туду
+    list.append(createToDo(newToDoInput.value));    // добавляем в форму новый toDo, аргумент - то что ввели в поле текстового импута
+    array.push(newToDoInput.value);                 // добавляем в массив текстовые значения toDo
+    localStorage.setItem("toDo", array.join(","));   // добавление в хранилище массива, преобразованного в строку (записываем массив в строку - serialisation)
+    newToDoInput.value = "";                        // очищение текстового поля 
 }
 
-//array.map(createToDo).forEach(el=>list.append(el));
-list.append(...array.map(createToDo))
+const clear = event => {    //функция очистки формы
+    list.remove();                                              // очищает форму
+    localStorage.clear()                // очищает хранилище
+}
 
-button.addEventListener("click", handleAdd);
+//array.map(el=>createToDo(el)).forEach(el=>list.append(el));
+list.append(...array.map(createToDo))               // добавляет в форму элемент(ы) из массива array, который(-ые) проходят преобразование функцией создания todo
+
+button.addEventListener("click", handleAdd);        // слушатель для кнопки добавить
+clearButton.addEventListener("click", clear);       // слушатель для кнопки удалить
 
 
 /*
-1. прокомментировать
-2. добавьте удаление всех элементов одной кнопкой (добавить ее в разметку "Очистить")
-3. добавить к каждому элементу кнопку "удалить", которая удаляет элемент
+1. v прокомментировать 
+2. v добавьте удаление всех элементов одной кнопкой (добавить ее в разметку "Очистить")
+3. v добавить к каждому элементу кнопку "удалить", которая удаляет элемент
 4.***** сохранять состояние чекбоксов (JSON.stringify, JSON.parse)
+
+JSON.stringify - civilization
 */
