36d7428430b2eefefbe0f7ffaf779614cb8a4728
Maria Angelova <angelova.m@hotmail.com>
2023-03-09 13:03:41 +0100

    fixed data storage method

--- Project-9-LocalStorage/index.js
+++ Project-9-LocalStorage/index.js
@@ -6,72 +6,95 @@ const clearButton = document.querySelector(".clear")                // кноп�
 const newToDoInput = document.querySelector('input[type="text"]');  // текстовое поле
 const list = document.querySelector("#list");                       // форма со списком todo
 
-const array = localStorage.getItem("toDo") ? localStorage.getItem("toDo").split(",") : []; //state
+const array = localStorage.getItem("toDo") ? JSON.parse(localStorage.getItem("toDo")) : []; //state
+const createToDoEntity = (textToDo) => ({ text: textToDo, isCompleted: false })
 
-const createToDo = (text) => {   //функция создания туду в разметке
+
+const createToDo = (toDoEntity) => {   //функция создания туду в разметке
     const newToDoContainer = document.createElement("label");   // создание лейбла 
     const newToDoValue = document.createElement("input");       // создания инпута для чекбокса
     const paragraph = document.createElement("p");              // создание p для текста
     const deleteButton = document.createElement("input");       // создание инпута для кнопки удаления
     
-    const dlt = event => {                                      // функция удаления элемента
-        newToDoContainer.remove();                              //удаление элемента
-        localStorage.setItem("toDo", array.filter(el => el !== paragraph.innerText).join(",")); //обновление хранилища
-        localStorage.removeItem(paragraph.innerText)
+    const dlt = event => {                                     // функция удаления элемента
+         
+        newToDoContainer.remove();                             //удаление элемента
+        // array.splice(0, array.length, ...array.filter(el => el !== paragraph.innerText));
+        const index = array.indexOf(toDoEntity);
+        (index !== -1) && (array.splice(index, 1));                                  
+        localStorage.setItem("toDo", JSON.stringify(array));     //обновление хранилища
     };
 
-    const changeHandler = event => {        //функция сохранения состояния чекбоксов
+    /*
+        const changeHandler = event => {                            
         if (newToDoValue.checked) {
             localStorage.setItem(paragraph.innerText, JSON.stringify(newToDoValue.checked)) //newToDoValue.checked  - true !!
         } else {
             localStorage.setItem(paragraph.innerText, false)
         }
     }
+    */
 
-    function winOnload() {                  // функция добавления сохраненных чекбоксов при загрузке страницы
-        newToDoValue.checked = localStorage.getItem(paragraph.innerText) === 'true' 
+    const handleChange = () => {                                //функция сохранения состояния чекбоксов
+        toDoEntity.isCompleted = !toDoEntity.isCompleted;
+        localStorage.setItem("toDo", JSON.stringify(array));
     }
 
-    window.addEventListener("load", winOnload)
-
-    paragraph.innerText = text;    // добавление текста внутрь элемента p ч/з переменную        
+    paragraph.innerText = toDoEntity.text;      
     
-    deleteButton.addEventListener("click", dlt);    // доб слушатель кнопке удаления с функцией удаления элемента
-    deleteButton.setAttribute("type", "button");    // доб атрибуты тип 
-    deleteButton.setAttribute("value", "delete");            // и значение
+    deleteButton.addEventListener("click", dlt);            // доб слушатель кнопке удаления с функцией удаления элемента
+    deleteButton.setAttribute("type", "button");            // доб атрибуты тип 
+    deleteButton.setAttribute("value", "delete");                  // и значение
     
-    newToDoContainer.append(newToDoValue, paragraph, deleteButton);// добавляем checkbox и p внутрь label-контейнер
-    newToDoValue.setAttribute("type", "checkbox");  // добавление атрибутов у инпутов
-    newToDoValue.addEventListener("change", changeHandler);
+    newToDoContainer.append(newToDoValue, paragraph, deleteButton); // добавляем checkbox и p внутрь label-контейнер
+    newToDoValue.setAttribute("type", "checkbox");                  // добавление атрибутов у инпутов
+    newToDoValue.checked = toDoEntity.isCompleted;
+    newToDoValue.addEventListener("change", handleChange);         //слушатель на событие change
 
-    return newToDoContainer; // возвращаем готовый label-контейнер;
+    return newToDoContainer;                        // возвращает готовый label-контейнер;
 }
 
 const handleAdd = event => { //функция добавления туду
-    list.append(createToDo(newToDoInput.value));    // добавляем в форму новый toDo, аргумент - то что ввели в поле текстового импута
-    array.push(newToDoInput.value);                 // добавляем в массив текстовые значения toDo
-    localStorage.setItem("toDo", array.join(","));   // добавление в хранилище массива, преобразованного в строку (записываем массив в строку - serialisation)
-    newToDoInput.value = "";                        // очищение текстового поля 
+    const toDoEntity = createToDoEntity(newToDoInput.value);
+    array.push(toDoEntity);
+    list.append(createToDo(toDoEntity));     // добавляем в форму новый toDo, аргумент - то что ввели в поле текстового импута
+    localStorage.setItem("toDo", JSON.stringify(array));   
+    newToDoInput.value = "";                         // очищение текстового поля 
 }
 
 const clear = event => {    //функция очистки формы
-    list.remove();                                              // очищает форму
-    localStorage.clear();                // очищает хранилище
-    location.reload();
+    /*
+    const collChildren = []
+    for(let i = 0; i < list.children.length; i++){
+        collChildren.push(list.children.item(i));
+    }
+    collChildren.forEach(e => e.remove())
+    
+    list.replaceChildren(); */
+   
+    while (list.firstChild) list.firstChild.remove(); /*list.removeChild(list.firstChild); */
+    array.splice(0,array.length);
+    localStorage.removeItem("toDo");                           // очищает хранилище
+    //location.reload();
 }
 
 //array.map(el=>createToDo(el)).forEach(el=>list.append(el));
-list.append(...array.map(createToDo))               // добавляет в форму элемент(ы) из массива array, который(-ые) проходят преобразование функцией создания todo
+list.append(...array.map(createToDo))            // добавляет в форму элемент(ы) из массива array, который(-ые) проходят преобразование функцией создания todo
 
-button.addEventListener("click", handleAdd);        // слушатель для кнопки добавить
-clearButton.addEventListener("click", clear);       // слушатель для кнопки удалить
+button.addEventListener("click", handleAdd);     // слушатель для кнопки добавить
+clearButton.addEventListener("click", clear);    // слушатель для кнопки удалить
 
+/*const toDoEntity = {
+    text: "some to do",
+    isCompleted: false
+}
 
+*/
 /*
+
 1. v прокомментировать 
 2. v добавьте удаление всех элементов одной кнопкой (добавить ее в разметку "Очистить")
 3. v добавить к каждому элементу кнопку "удалить", которая удаляет элемент
-4.***** сохранять состояние чекбоксов (JSON.stringify, JSON.parse)
+4. v сохранять состояние чекбоксов (JSON.stringify, JSON.parse)
 
-JSON.stringify - civilization
 */
