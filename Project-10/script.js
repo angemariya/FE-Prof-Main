e2ca035b70d54ce1f50a2b32616522cc33e0a296
Maria Angelova <angelova.m@hotmail.com>
2023-03-21 14:49:18 +0100

    Add MVC Model

--- Project-10/script.js
+++ Project-10/script.js
@@ -1,25 +1,35 @@
 //model
-const state = {
-  array: [
+const arrImage =  [
     "https://fastly.picsum.photos/id/282/200/300.jpg?hmac=CVnHWoW4uSir_3zYiJzNPVs8XMSyf968WjkWvPlwu3g", //доменная модель
     "https://fastly.picsum.photos/id/663/200/300.jpg?hmac=OYPBwsRmaygvAiTN0M4ZNNWBZqgbTGuH2aXkJ4FLX_M",
     "https://fastly.picsum.photos/id/1035/200/300.jpg?hmac=744aBtkMLjfDyn2TzkMxsFzw2T0L57TMlNGFlX-Qgq0",
     "https://fastly.picsum.photos/id/171/200/300.jpg?hmac=NHia9vzbBwrKnBFwp7cDZPSxFcVF_VGbnFO5LAjWnuE",
     "https://fastly.picsum.photos/id/841/200/300.jpg?hmac=G9hBg_h2jvXDwBgnqCm8LO9PXRrPRWbz1xgdUrMf1Y8",
-  ],
-  idEl: 0,
-};
+  ]
 
-const state2 = {
-    array: [
-      "https://fastly.picsum.photos/id/282/200/300.jpg?hmac=CVnHWoW4uSir_3zYiJzNPVs8XMSyf968WjkWvPlwu3g", //доменная модель
-      "https://fastly.picsum.photos/id/663/200/300.jpg?hmac=OYPBwsRmaygvAiTN0M4ZNNWBZqgbTGuH2aXkJ4FLX_M",
-      "https://fastly.picsum.photos/id/1035/200/300.jpg?hmac=744aBtkMLjfDyn2TzkMxsFzw2T0L57TMlNGFlX-Qgq0",
-      "https://fastly.picsum.photos/id/171/200/300.jpg?hmac=NHia9vzbBwrKnBFwp7cDZPSxFcVF_VGbnFO5LAjWnuE",
-      "https://fastly.picsum.photos/id/841/200/300.jpg?hmac=G9hBg_h2jvXDwBgnqCm8LO9PXRrPRWbz1xgdUrMf1Y8",
-    ],
-    idEl: 0,
-  };
+  const arrImage2 =  [
+    "https://fastly.picsum.photos/id/282/200/300.jpg?hmac=CVnHWoW4uSir_3zYiJzNPVs8XMSyf968WjkWvPlwu3g", //доменная модель
+    "https://fastly.picsum.photos/id/663/200/300.jpg?hmac=OYPBwsRmaygvAiTN0M4ZNNWBZqgbTGuH2aXkJ4FLX_M",
+    "https://fastly.picsum.photos/id/1035/200/300.jpg?hmac=744aBtkMLjfDyn2TzkMxsFzw2T0L57TMlNGFlX-Qgq0",
+    "https://fastly.picsum.photos/id/171/200/300.jpg?hmac=NHia9vzbBwrKnBFwp7cDZPSxFcVF_VGbnFO5LAjWnuE",
+    "https://fastly.picsum.photos/id/841/200/300.jpg?hmac=G9hBg_h2jvXDwBgnqCm8LO9PXRrPRWbz1xgdUrMf1Y8",
+  ]
+
+  const arrImage3 =  [
+    "https://fastly.picsum.photos/id/282/200/300.jpg?hmac=CVnHWoW4uSir_3zYiJzNPVs8XMSyf968WjkWvPlwu3g", //доменная модель
+    "https://fastly.picsum.photos/id/663/200/300.jpg?hmac=OYPBwsRmaygvAiTN0M4ZNNWBZqgbTGuH2aXkJ4FLX_M",
+    "https://fastly.picsum.photos/id/1035/200/300.jpg?hmac=744aBtkMLjfDyn2TzkMxsFzw2T0L57TMlNGFlX-Qgq0",
+    "https://fastly.picsum.photos/id/171/200/300.jpg?hmac=NHia9vzbBwrKnBFwp7cDZPSxFcVF_VGbnFO5LAjWnuE",
+    "https://fastly.picsum.photos/id/841/200/300.jpg?hmac=G9hBg_h2jvXDwBgnqCm8LO9PXRrPRWbz1xgdUrMf1Y8",
+  ]
+
+function createState (arr, idEl = 0) {
+  const state = {
+    array: arr,
+    idEl: idEl
+  }
+  return state;
+}
 
 //view
 
@@ -31,35 +41,39 @@ const render = (root, localState) => {
 };
 
 //controller
-const findElements = (suffix) = {
-    triangleLeft: document.querySelector(".triangle`${suffix}`.left"),
-    triangleRight: document.querySelector(".triangle`${suffix}`.right"),
-    boxForImage: document.querySelector(".boxForImage`${suffix}`")       //root
-}
+
+const findElements = (suffix) => ({
+  triangleLeft: document.querySelector(`.triangle${suffix}.left`),
+  triangleRight: document.querySelector(`.triangle${suffix}.right`),
+  boxForImage: document.querySelector(`.boxForImage${suffix}`)             //root
+});
 
 const createActions = (box, localState) => ({
-  start: () => render(box, state),
+  start: () => render(box, localState),
 
   moveLeft: () => {
-    localState.idEl = localState.idEl === 0 ? localState.array.length - 1 : localState.idEl - 1;
-    render(box, state);
+    localState.idEl =
+      localState.idEl === 0 ? localState.array.length - 1 : localState.idEl - 1;
+    render(box, localState);
   },
 
   moveRight: () => {
-    localState.idEl = localState.idEl === localState.array.length - 1 ? 0 : localState.idEl + 1;
-    render(box, state);
+    localState.idEl =
+      localState.idEl === localState.array.length - 1 ? 0 : localState.idEl + 1;
+    render(box, localState);
   },
 });
 
 const sliderComponate = (state, suffix) => {
-    const actions = createActions(elements.boxForImage, state);
-    const elements = findElements(suffix);
-    elements.triangleLeft.addEventListener("click", actions.moveLeft);
-    elements.triangleRight.addEventListener("click", actions.moveRight);
-    actions.start();
-}
+  const elements = findElements(suffix);
+  const actions = createActions(elements.boxForImage, state);
+  elements.triangleLeft.addEventListener("click", actions.moveLeft);
+  elements.triangleRight.addEventListener("click", actions.moveRight);
+  actions.start();
+};
 
-sliderComponate(state, "")
-sliderComponate(state2, "2")
+sliderComponate(createState(arrImage), "");
+sliderComponate(createState(arrImage2), "2");
+sliderComponate(createState(arrImage3), "3");
 
 //localState - объект состояния
