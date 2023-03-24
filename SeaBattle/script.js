c0b523fa4235ef646e5cf09ffdd02856fc6a736d
Maria Angelova <angelova.m@hotmail.com>
2023-03-24 09:24:17 +0100

    Add changing color by click

--- SeaBattle/script.js
+++ SeaBattle/script.js
@@ -8,38 +8,66 @@ const state = {
 
 //тут мб функции к-е работают с данными
 
-//view
-
-const calculateElements = (root, localState, el) => {
-    //вычислим, что мы отрисовываем исходя из нашего состояния
-    const divArray = [];
-    for (let j = 0; j < 10; j++) {        
-        for (let i = 0; i < 10; i++) {
-            const divRow = document.createElement("div");
-            divRow.className = "field";
-            divRow.addEventListener("click", el.getCoordinate)
-            divArray.push(divRow);
+const createState = () => {
+  const createSquare = (x,y) => ({
+    x: x,
+    y: y,
+    status: false
+  })
+    for (let j = 1; j <= 10; j++) {        
+        for (let i = 1; i <= 10; i++) {
+            state.entities.push(createSquare(i,j))
         }
     }
-    return divArray;
-};
+    return state;
+}
+createState();
 
-const render = (localState, root) => {
-  const elements = calculateElements(localState); // вычисляем, что мы рисуем, записываем в переменную elements
+//view
+
+const calculateElements = (localState, actions) => {
+    //вычислим, что мы отрисовываем исходя из нашего состоянии
+    const template = localState.entities.map(el=>{
+      const div = document.createElement("div");
+      div.classList.add("field");
+      div.addEventListener("click", ()=>actions.getCoordinate(el))
+      div.addEventListener("click", ()=>actions.changeStatus(el))
+      if (el.status) {
+        div.style.backgroundColor = "green";
+      }
+      return div;
+    })
+    return template;
+}
+
+const render = (localState, root, actions) => {  // вычисляем, что мы рисуем, записываем в переменную elements
+  const elements = calculateElements(localState, actions);
   root.replaceChildren();
   root.append(...elements);
+
 };
 //controller
 
-const getActions = (localState, root) => ({
-  start: () => {
-    render(localState, root);
-  },
-  getCoordinate: () => {
-    console.log([i+1], [j+1])
+const getActions = (localState, root) => {
+  const card = {
+  start: () => {render(localState, root, card)},
+  
+  getCoordinate:(x) => {console.log([x])},
+  
+  changeStatus: (square) => {
+    square.status = !square.status;
+    render(localState, root, card);
   }
-});
+  }
+  return card;
+};
 
 const root = document.querySelector("#root");
 const actions = getActions(state, root);
 actions.start();
+
+
+// сделать отрисовку из модели
+// хранить в модели состояние клеточек "нажата" "не нажата"
+// нажатую выделять цветом в отрисовке
+// при нажатии менять состояние клетки
