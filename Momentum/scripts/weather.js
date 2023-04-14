5169c20ac8489f3dc2438f01b7988c7e61af33d5
Maria Angelova <angelova.m@hotmail.com>
2023-04-14 10:50:05 +0200

    Add style for weather

--- Momentum/scripts/weather.js
+++ Momentum/scripts/weather.js
@@ -1,9 +1,21 @@
 
+const weatherContainer = document.querySelector('#weather');
+
+const createWeatherWidget = (temp, location) => {
+    const tempText = document.createElement("span");
+    tempText.innerHTML = temp;
+    const locationText = document.createElement("p");
+    locationText.innerHTML = location;
+    weatherContainer.append(tempText, locationText);
+    return weatherContainer;
+}
+
 const fetchData = async (lat,lon) => {
     let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=abd5532c6680b914cee9e0c74819f228&units=metric`);
     let data = await weather.json();
-    
-    console.log(Math.floor(data.main.temp)+"°С", data.name);
+    const temp = Math.floor(data.main.temp)+"°С";
+    const location = data.name;
+    createWeatherWidget(temp, location);
 }
 
 const getPosition = () => {
