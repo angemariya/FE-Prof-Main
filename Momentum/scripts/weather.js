6ba1c2a45b56ec9abdcbda9825faeb6c3deb57d0
Maria Angelova <angelova.m@hotmail.com>
2023-04-14 10:36:58 +0200

    Add weather API

--- Momentum/scripts/weather.js
+++ Momentum/scripts/weather.js
@@ -1,19 +1,24 @@
+
 const fetchData = async (lat,lon) => {
-    let weather = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${abd5532c6680b914cee9e0c74819f228}`);
+    let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=abd5532c6680b914cee9e0c74819f228&units=metric`);
     let data = await weather.json();
-    return data;
+    
+    console.log(Math.floor(data.main.temp)+"°С", data.name);
 }
 
-const geoSuccessHandler = (position) => {
-
+const getPosition = () => {
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 
-const geoErrorHadler = () => {
+const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
 
+    fetchData(latitude, longitude);
 }
 
-const getCoordinate = () => {
-    console.log(navigator.geolocation.getCurrentPosition(geoSuccessHandler, geoErrorHadler));
+const error = () => {
+    console.log("Error");
 }
 
-getCoordinate();
+getPosition();
