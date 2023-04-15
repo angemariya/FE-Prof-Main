bed23557e697f3eb4d6c3d0d4ebffafdec19ec43
Maria Angelova <angelova.m@hotmail.com>
2023-04-15 11:07:20 +0200

    Add saving coords to LocalStorage

--- Momentum/scripts/weather.js
+++ Momentum/scripts/weather.js
@@ -1,4 +1,11 @@
-const coordsState = localStorage.getItem("location") ? JSON.parse(localStorage.getItem("location")) : {}
+let coordsState = JSON.parse(localStorage.getItem("location")) || {};
+
+function saveToState(latitude, longitude) {
+    return coordsState = {
+        latitude,
+        longitude
+    }
+}
 
 const fetchData = async (lat,lon) => {
     let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=abd5532c6680b914cee9e0c74819f228&units=metric`);
@@ -26,22 +33,23 @@ const saveToLocalStorage = (obj) => {
     localStorage.setItem("location", JSON.stringify(obj))
 }
 
-const getPosition = () => {
+function getPosition () {
     navigator.geolocation.getCurrentPosition(success, error);
 }
 
-const success = (position) => {
+function success (position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     saveToLocalStorage({latitude, longitude});
+    saveToState(latitude, longitude)
     fetchData(coordsState.latitude, coordsState.longitude)
 }
 
-const error = () => {
-    fetchData(coordsState.latitude, coordsState.longitude) || console.log("Error");
+function error () {
+    console.log("Error");
 }
-
 getPosition();
 
-//const coordsState = localStorage.getItem("location") ? JSON.parse(localStorage.getItem("location")) : getPosition();
+
+// http://dataservice.accuweather.com/forecasts/v1/daily/1day/sVjOLnlwq51hE3UfL5qlnuMysh1V8EqG&metric=true 
 //if there is no data in localStorage getPosition() will be invoke, else data will be parsed from LocalStorage
