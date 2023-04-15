13d33683eabeedbd01001a20a53216591eb3b4bb
Maria Angelova <angelova.m@hotmail.com>
2023-04-15 09:43:21 +0200

    Add saving coords in localStorage

--- Momentum/scripts/weather.js
+++ Momentum/scripts/weather.js
@@ -1,18 +1,4 @@
-
-const weatherContainer = document.querySelector('#weather');
-
-const createWeatherWidget = (temp, location) => {
-    const tempText = document.createElement("span");
-    tempText.innerHTML = temp;
-    const locationText = document.createElement("p");
-    locationText.innerHTML = location;
-    weatherContainer.append(tempText, locationText);
-    return weatherContainer;
-}
-
-const saveToLocalStorage = (obj) => {
-    localStorage.setItem("")
-}
+const coordsState = localStorage.getItem("location") ? JSON.parse(localStorage.getItem("location")) : {}
 
 const fetchData = async (lat,lon) => {
     let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=abd5532c6680b914cee9e0c74819f228&units=metric`);
@@ -20,7 +6,24 @@ const fetchData = async (lat,lon) => {
     const temp = Math.floor(data.main.temp)+"°С";
     const location = data.name;
     createWeatherWidget(temp, location);
+}
 
+const weatherContainer = document.querySelector('#weather');
+
+const createWeatherWidget = (temp, location) => {
+
+    const tempText = document.createElement("p");
+    tempText.classList.add("temperature");
+    tempText.innerHTML = `Wetter today: ${temp}`;
+    const locationText = document.createElement("p");
+    locationText.classList.add("location")
+    locationText.innerHTML = location;
+    weatherContainer.append(tempText, locationText);
+    return weatherContainer;
+}
+
+const saveToLocalStorage = (obj) => {
+    localStorage.setItem("location", JSON.stringify(obj))
 }
 
 const getPosition = () => {
@@ -30,12 +33,15 @@ const getPosition = () => {
 const success = (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    fetchData(latitude, longitude)
+    saveToLocalStorage({latitude, longitude});
+    fetchData(coordsState.latitude, coordsState.longitude)
 }
 
-
 const error = () => {
-    console.log("Error");
+    fetchData(coordsState.latitude, coordsState.longitude) || console.log("Error");
 }
 
 getPosition();
+
+//const coordsState = localStorage.getItem("location") ? JSON.parse(localStorage.getItem("location")) : getPosition();
+//if there is no data in localStorage getPosition() will be invoke, else data will be parsed from LocalStorage
